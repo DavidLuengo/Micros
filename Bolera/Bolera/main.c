e79153422b79c417e5e7b6f5581de44ee2c4df0c
DavidLuengo <38347465+DavidLuengo@users.noreply.github.com>
2018-05-04 14:02:25 +0200

    Update main.c

--- Bolera/Bolera/main.c
+++ Bolera/Bolera/main.c
@@ -5,31 +5,37 @@
  * Author : EquipoK
  */ 
 
+// Puerto J1:
+//	Puerto B: motores 1, 3, 4, 5 (bajo: enable, alto: dir)
+//	Puerto L: motor 2 (enable 0, dir 1, bk 2)
+//	Puerto K: interrupts
+
 //	LIBRERIAS
 #include <avr/io.h>
 
 //	DEFINICIONES, ESTRUCTURAS y CONSTANTES
-const uint8_t DCHA = 1;		// DERECHA
-const uint8_t IZDA = 0;		// IZQUIERDA
-const uint8_t ON = 1;		// ENABLE ON
-const uint8_t OFF = 0;		// ENABLE OFF
-const uint8_t ACT = 1;		// BK ACTIVO (solo motor2)
-const uint8_t DEACT = 0;	// BK INACTIVO (solo motor2)
+const uint8_t DCHA = 0x01;		// DERECHA
+const uint8_t IZDA = 0x00;		// IZQUIERDA
+const uint8_t ON = 0x01;		// ENABLE ON
+const uint8_t OFF = 0x00;		// ENABLE OFF
+const uint8_t ACT = 0x01;		// BK ACTIVO (solo motor2)
+const uint8_t DEACT = 0x00;	// BK INACTIVO (solo motor2)
 
 typedef struct{
 	volatile uint8_t * port;
 	uint8_t enable;
 	uint8_t dir;
 	uint8_t bk;
+	uint8_t index; // Indica el bit que usa para su señal de enable
 	//A lo mejor hay que añadir una variable de velocidad
 } motor;
 
 //	VARIABLES
-motor motor1;
-motor motor2;
-motor motor3;
-motor motor4;
-motor motor5;
+motor motor1 = {&PORTB, 0, 0, 0, 0};
+motor motor2 = {&PORTL, 0, 0, 0, 0};
+motor motor3 = {&PORTB, 0, 0, 0, 2};
+motor motor4 = {&PORTB, 0, 0, 0, 4};
+motor motor5 = {&PORTB, 0, 0, 0, 6};
 
 	// Vector de direcciones a los motores
 motor *motores[] = {&motor1, &motor2, &motor3, &motor4, &motor5};
@@ -51,9 +57,43 @@ void moveMotor(motor* M, uint8_t direccion){
 	// Esta funcion mueve en la direccion asignada un motor
 	// Ejemplo:	moveMotor(motores[1],0);
 	//		moveMotor(&motor1,0);
-	M->enable = ON;
-	M->bk = DEACT;	// Esto puede no estar bien
-	M->dir = direccion;
+	
+	uint8_t aux = 0;
+	uint8_t mask = 0b00000011;
+	
+	
+	if (M->port == &PORTL){
+		M->enable = ON;
+		M->bk = DEACT;	// Esto puede no estar bien
+		M->dir = direccion;
+		
+		aux = (bk<<2)|(direccion<<1)|enable;
+		
+		mask = (mask<<1)|0x01;
+		mask = ~mask;
+		
+		//PORTL &= mask;
+		//PORTL |= aux;
+		
+	}
+	else if (M->port == &PORTB){
+		M->dir = direccion;
+		M->enable = ON;
+		
+		mask = mask<<(M->index);
+		mask = ~mask;
+		
+		//aux = (direccion*2 + enable)*2^(M->index);
+		aux = (direccion<<1)|enable;
+		aux = aux<<(M->index);
+		
+		//PORTB &= mask; // Deja libres los bits
+		//PORTB |= aux;
+	}
+	
+	M->port* &=mask;
+	M->port* |= aux;
+	
 }
 
 void Dinamicstop(void){
