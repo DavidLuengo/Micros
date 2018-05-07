f399982302218f102d81cb39c58a3a33f69c3c98
DavidLuengo <38347465+DavidLuengo@users.noreply.github.com>
2018-05-07 21:07:03 +0200

    Update main.c

--- Bolera/Bolera/main.c
+++ Bolera/Bolera/main.c
@@ -19,7 +19,7 @@ const uint8_t IZDA = 0x00;		// IZQUIERDA
 const uint8_t ON = 0x01;		// ENABLE ON
 const uint8_t OFF = 0x00;		// ENABLE OFF
 const uint8_t ACT = 0x01;		// BK ACTIVO (solo motor2)
-const uint8_t DEACT = 0x00;	// BK INACTIVO (solo motor2)
+const uint8_t DEACT = 0x00;		// BK INACTIVO (solo motor2)
 
 typedef struct{
 	volatile uint8_t * port;
@@ -40,6 +40,9 @@ motor motor5 = {&PORTB, 0, 0, 0, 6};
 	// Vector de direcciones a los motores
 motor *motores[] = {&motor1, &motor2, &motor3, &motor4, &motor5};
 
+	// Variable de oscilacion. Vuelve a 0 cuando se pulsa SW6 para disparar
+uint8_t *swing = 0x00;
+
 //	FUNCIONES
 
 void delay(int ms){
@@ -54,7 +57,7 @@ void moveMotor(motor* M, uint8_t direccion){
 	// Ejemplo:	moveMotor(motores[1],0);
 	//		moveMotor(&motor1,0);
 	
-	uint8_t aux = 0;
+	uint8_t aux = 0x00;
 	uint8_t mask = 0b00000011;
 	
 	
@@ -68,8 +71,8 @@ void moveMotor(motor* M, uint8_t direccion){
 		mask = (mask<<1)|0x01;
 		mask = ~mask;
 		
-		//PORTL &= mask;
-		//PORTL |= aux;
+		PORTL &= mask;
+		PORTL |= aux;
 		
 	}
 	else if (M->port == &PORTB){
@@ -79,35 +82,61 @@ void moveMotor(motor* M, uint8_t direccion){
 		mask = mask<<(M->index);
 		mask = ~mask;
 		
-		//aux = (direccion*2 + enable)*2^(M->index);
 		aux = (direccion<<1)|M->enable;
 		aux = aux<<(M->index);
 		
-		//PORTB &= mask; // Deja libres los bits
-		//PORTB |= aux;
+		PORTB &= mask; // Deja libres los bits
+		PORTB |= aux;
 	}
-	
-	*M->port &= mask;
-	*M->port |= aux;
-	
 }
 
-void setup_nuestro(void){
-	moveMotor(motores[2],IZDA);
-	moveMotor(motores[3],IZDA);
-	moveMotor(motores[3],DCHA);
+void stopMotor(motor* M){
+	uint8_t aux = 0x00;
+	uint8_t mask = 0b00000011;
 	
-	delay(3000);
 	
-	//Esperamos un tiempo a que todos los SW estén pulsados.
+	if (M->port == &PORTL){
+		M->enable = OFF;
+		M->bk = ACT;
+		
+		aux = (M->bk<<2)|(M->dir<<1)|M->enable;
+		
+		mask = (mask<<1)|0x01;
+		mask = ~mask;
+		
+		PORTL &= mask;
+		PORTL |= aux;
+		
+	}
+	else if (M->port == &PORTB){
+		M->enable = OFF;
+		
+		mask = mask<<(M->index);
+		mask = ~mask;
+		
+		aux = (direccion<<1)|M->enable;
+		aux = aux<<(M->index);
+		
+		PORTB &= mask; // Deja libres los bits
+		PORTB |= aux;
+	}
+}
+
+void swingDelay(){
+	//Esta es una version de la funcion de oscilacion usando delay
+	*swing = 0x01;
+	while(*swing == 0x01){
+		moveMotor(&motor2,!&motor2->dir);
+		delay(2000);
+		stopMotor(&motor2);
+		delay(50);
+	}
 }
 
 void setup(void){
 	//Pone en marcha todos los motores hacia su posición de inicio.
 	//Esperar un tiempo
 	//Cuando todos se encuentren en su posicion original se carga la primera bola
-	
-	
 }
 
 
@@ -140,14 +169,12 @@ ISR(){
 
 //	PROGRAMA PRINCIPAL
 int main(void)
-{
-    setup_nuestro();
+{	
+	setup();
 	
-	
-	
-    while (1) 
-    {
-	    // Bucle principal
-    }
+   	while (1) 
+    	{
+		// Bucle principal
+    	}
 }
 
